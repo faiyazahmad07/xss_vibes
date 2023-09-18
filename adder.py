1c7832f3448a22b0a40fc1b63d9d3a90a6b951c7
@hacktivator <63125338+faiyazahmad07@users.noreply.github.com>
2023-09-18 22:36:13 +0530

    Update adder.py
    new adder.py
    -> Attributes will get set automatically

--- adder.py
+++ adder.py
@@ -1,12 +1,66 @@
+from optparse import OptionParser
 import json
-payload = input("Enter the payload: ")
-attributes = input("Enter the attributes(Separate by spaces): ")
 
-new_data = {"Payload": payload, "Attribute": attributes.split(" "),"count":0}
+class Adder:
+    def __init__(self):
+        self.dangerous_characters = [  # You can add dangerous characters here
+            ">",
+            "'",
+            '"',
+            "<",
+            "/",
+            ";"
+        ]
 
-f = open('payloads.json')
-data = json.load(f)
-data.append(new_data)
-with open('payloads.json',"w") as write_data:
-    json.dump(data,write_data,indent=4)
-print("[+] PAYLOAD HAS BEEN ADDED")
+    def add_payload(self,payload=None,filename=None):
+        if filename:
+            with open(val.filename, 'r') as payloads:
+                payloads = payloads.readlines()
+            for payload in payloads:
+                new_data = {
+                    "Payload": payload,
+                    "Attribute": [],
+                    "count": 0,
+                    "waf": val.waf
+                }
+                for char in payload:
+                    if char in self.dangerous_characters:
+                        if char in new_data['Attribute']:
+                            pass
+                        else:
+                            new_data['Attribute'].append(char)
+                f = open('payloads.json')
+                data = json.load(f)
+                data.append(new_data)
+                with open('payloads.json', "w") as write_data:
+                    json.dump(data, write_data, indent=4)
+            print("[+] PAYLOAD HAS BEEN ADDED")
+
+        else:
+            new_data = {
+                "Payload": val.payload,
+                "Attribute": [],
+                "count": 0,
+                "waf": val.waf
+            }
+            for char in val.payload:
+                if char in self.dangerous_characters:
+                    new_data['Attribute'].append(char)
+
+            # print(new_data)
+
+            f = open('payloads.json')
+            data = json.load(f)
+            data.append(new_data)
+            with open('payloads.json', "w") as write_data:
+                json.dump(data, write_data, indent=4)
+            print("[+] PAYLOAD HAS BEEN ADDED")
+
+
+if __name__ == "__main__":
+    parser = OptionParser()
+    parser.add_option("-p", dest="payload", help="enter payload")
+    parser.add_option("-f", dest="filename", help="enter filename containing all the payloads")
+    parser.add_option("-w", dest="waf", help="enter waf name")
+    val, args = parser.parse_args()
+    Adder().add_payload(filename=val.filename,payload=val.payload)
